9c614a2238ea78c4e941fb441b53761342e08bde
TheKittehJesus <29379890+TheKittehJesus@users.noreply.github.com>
2025-06-01 12:52:33 -0500

    Now iterates in reverse order

--- Content.Shared/_CP14/Skill/CP14SharedSkillSystem.cs
+++ Content.Shared/_CP14/Skill/CP14SharedSkillSystem.cs
@@ -254,12 +254,12 @@ public abstract partial class CP14SharedSkillSystem : EntitySystem
         {
             return false;
         }
-
-        foreach (var skill in from skill in component.LearnedSkills
-                              where !HaveFreeSkill(target, skill, component)
-                              select skill)
+        for(var i = component.LearnedSkills.Count-1;i >= 0; i--)
         {
-            TryRemoveSkill(target, skill, component);
+            if(!HaveFreeSkill(target, component.LearnedSkills[i]))
+            {
+                TryRemoveSkill(target, component.LearnedSkills[i], component);
+            }
         }
         return true;
     }
